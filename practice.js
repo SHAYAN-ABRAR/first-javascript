d47720d8453da93632bac2c8eb857bbd37e6adf7
SHAYAN ABRAR <shayanabrar7@gmail.com>
2024-01-30 00:31:55 +0600

    first commit

---
+++ practice.js
@@ -0,0 +1,24 @@
+//1st task
+let x = 1000;
+let price = 700;
+let returnValue = x - price;
+//console.log(returnValue);
+
+
+//2nd task
+let math=75.25;
+let bio=65;
+let chem=80;
+let phy=35.45;
+let ban=99.50;
+
+let total= (math + bio + chem + phy + ban)/5;
+//console.log(total.toFixed(2));
+
+
+//3rd task
+let div=119;
+
+let divAns= 119%5;
+console.log(divAns);
+
